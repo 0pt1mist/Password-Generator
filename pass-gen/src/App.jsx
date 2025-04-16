dd11dcb58e053211636a032ef64f9097802ceeca
DrynovEgor <drynovegorka@gmail.com>
2025-04-16 16:14:45 +0700

    Егор
    Исправил App.jsx. Добавил переменные для передачи данных в компоненты

--- pass-gen/src/App.jsx
+++ pass-gen/src/App.jsx
@@ -1,14 +1,21 @@
 import { Header } from './components/header.jsx';
 import { HeadInfo } from './components/head-info.jsx';
 import { MainWin } from './components/main-app.jsx';
+import { GenPassFormF } from './components/form.jsx';
+import { PasswordList } from './components/passwordList.jsx';
 
+import { useState } from 'react';
 
 function App() {
+
+  const [allGeneratedPass, setAllGeneratedPass] = useState('')
+
   return (
     <div className="App">
       <Header />
       <HeadInfo />
-      <MainWin />
+      <GenPassFormF allGeneratedPass={allGeneratedPass} setAllGeneratedPass={setAllGeneratedPass} />
+      <PasswordList allGeneratedPass={allGeneratedPass} />
     </div>
   );
 }
